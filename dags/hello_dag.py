b2c03d6c79e47d2ea0f118625f40ab04b4efddf6
Karin <karinversan@gmail.com>
2026-01-27 21:09:15 +0300

    infra: add airflow service

---
+++ dags/hello_dag.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Hello from Airflow!")
+
+with DAG(
+    dag_id="hello_airflow",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,  # запуск вручную
+    catchup=False,
+) as dag:
+
+    t1 = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello
+    )
